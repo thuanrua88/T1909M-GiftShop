90ff5d12df03b37d1b2e6282aae9869011836447
thuanrua88 <60793480+thuanrua88@users.noreply.github.com>
2020-02-12 23:16:51 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+function openmenu() {
+  document.getElementById("side-menu").style.display="block";
+  document.getElementById("start-menu-btn").style.display="none";
+  document.getElementById("close-menu-btn").style.display="block";
+
+}
+function closemenu() {
+  document.getElementById("side-menu").style.display="none";
+  document.getElementById("start-menu-btn").style.display="block";
+  document.getElementById("close-menu-btn").style.display="none";
+
+}
